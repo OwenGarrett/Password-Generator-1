4a907da6d4e7ff65ff89892ed269d7168af1975c
Owen Garrett <owengarrett@Owens-Air.lan>
2022-12-28 14:56:25 -0500

    redid js

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,58 @@
+var characterLength = 8;
+var choiceArr = [];
+
+var lowercaseAlphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var uppercaseAlphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numbers = ['0','1','2','3','4','5','6','7','8','9'];
+var specialCharacters = ['`','~','!','@','#','$','%','^','&','*','(',')','-','_','+','='];
+
+
 // Assignment code here
+var generateBtn = document.querySelector("#generate");
 
-/* Query Selectors
- var generateBtn = document.querySelector("#generate");
- var password = document.querySelector("#password");
- 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
-Variables for password
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+function generatePassword() {
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Assignment code here
+//Query Selectors
+var generateBtn = document.querySelector("#generate");
+var password = document.querySelector("#password");
+
+// Variables for password
 var lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var specialCharacters = "~`!@#$%^&*()-_=+[{]},.<>/?;:";
 
-
-Event listener 
- generateBtn.addEventListener("click, generatePassword");
-*/
+// Event listener
+// generateBtn.addEventListener("click, generatePassword");
 
 // Prompt
 function generatePassword() {
@@ -37,7 +75,7 @@ function generatePassword() {
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -45,8 +83,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+*/
