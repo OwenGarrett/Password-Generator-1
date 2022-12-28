de79247df48d72ea03c9a91660cb7b310c966a6b
Owen Garrett <owengarrett@Owens-Air.lan>
2022-12-28 13:04:27 -0500

    created prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,36 @@
 // Assignment code here
+//Query Selectors
+var generateBtn = document.querySelector("#generate");
+var password = document.querySelector("#password");
 
+// Variables for password
+var lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialCharacters = "~`!@#$%^&*()-_=+[{]},.<>/?;:";
+
+// Prompt
+function generatePassword() {
+  var length = parseInt(
+    prompt(
+      "Please enter the length of your password. Password must be between 8 and 128 characters."
+    )
+  );
+  if (!isNaN(length)) {
+    if (length > 8 && length < 128) {
+      console.log("correct length");
+    } else {
+      console.log("incorrect length");
+    }
+  } else {
+    console.log("not a number");
+  }
+
+  return "Hello World";
+}
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
